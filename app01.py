c3479bb6abb990302fed28f76ef6b81b4000f729
slimefarm <slimefarm0304@gmail.com>
2021-08-13 07:55:36 +0900

    commit2108130755

--- app01.py
+++ app01.py
@@ -4,3 +4,8 @@ import hashlib
 
 st.title("SlimeFarm SampleSite")
 st.subheader("Login Site")
+
+from __future__ import absolute_import, unicode_literals
+import os
+
+print(os.environ.get('PASSWORD'))
