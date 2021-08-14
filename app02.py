3a2958673a0bf9ba381746185bc9c1677d61b3c7
slimefarm <slimefarm0304@gmail.com>
2021-08-14 12:08:01 +0900

    2108141207

--- app02.py
+++ app02.py
@@ -1,3 +1,4 @@
+from email.mime import text
 import streamlit as st
 import smtplib
 import os
@@ -12,13 +13,29 @@ def app():
  
     st.subheader("Delivery address")
     #left_column,right_column = st.columns(2)
-    left_column,right_column = st.columns([1,5])
+    left_column,right_column = st.columns([1,4])
     select = left_column.selectbox("Reji or New",(" ","Reji","New"))
     if select == "Reji":
         to_address = right_column.selectbox('Pls select mail-address',(" ",'tomotaka.a@gmail.com','slimefarm0304@gmail.com','style.azuma@gmail.com'))
     elif select == "New":
         to_address = right_column.text_input('Pls input mail-address')
 
+    st.subheader("Select file")
+    left_column, center_column, right_column = st.columns(3)
+    file_01 = left_column.checkbox('samplefile_01')
+    file_02 = left_column.checkbox('samplefile_02')
+    file_03 = left_column.checkbox('samplefile_03')
+    file_04 = left_column.checkbox('samplefile_04')
+
+    file_05 = center_column.checkbox('samplefile_05')
+    file_06 = center_column.checkbox('samplefile_06')
+    file_07 = center_column.checkbox('samplefile_07')
+    file_08 = center_column.checkbox('samplefile_08')
+
+    file_09 = right_column.checkbox('samplefile_09')
+    file_10 = right_column.checkbox('samplefile_10')
+    file_11 = right_column.checkbox('samplefile_11')
+    file_12 = right_column.checkbox('samplefile_12')
 
     res = st.button("send")
     if res ==True:
@@ -34,8 +51,32 @@ def app():
         from_address = smtp_user
         subject = "タイトル"
         body = "本文"
-        filepath = "./sample.txt"
-        filename = os.path.basename(filepath)
+        #filepath = "./sample.txt"
+        #filename = os.path.basename(filepath)
+        filepath01 ="./sample_01.txt"
+        filename01 = os.path.basename(filepath01)
+        filepath02 ="./sample_02.txt"
+        filename02 = os.path.basename(filepath02)
+        filepath03 ="./sample_03.txt"
+        filename03 = os.path.basename(filepath03)
+        filepath04 ="./sample_04.txt"
+        filename04 = os.path.basename(filepath04)
+        filepath05 ="./sample_05.txt"
+        filename05 = os.path.basename(filepath05)
+        filepath06 ="./sample_06.txt"
+        filename06 = os.path.basename(filepath06)
+        filepath07 ="./sample_07.txt"
+        filename07 = os.path.basename(filepath07)
+        filepath08 ="./sample_08.txt"
+        filename08 = os.path.basename(filepath08)
+        filepath09 ="./sample_09.txt"
+        filename09 = os.path.basename(filepath09)
+        filepath10 ="./sample_10.txt"
+        filename10 = os.path.basename(filepath10)
+        filepath11 ="./sample_11.txt"
+        filename11 = os.path.basename(filepath11)
+        filepath12 ="./sample_12.txt"
+        filename12 = os.path.basename(filepath12)
 
         #メール文書を送付用データ（添付ファイル付）を生成
         msg = MIMEMultipart()
@@ -43,10 +84,70 @@ def app():
         msg["From"] = from_address
         msg["To"] = to_address
         msg.attach(MIMEText(body))
-        with open(filepath, "rb") as f:
-            mb = MIMEApplication(f.read())
-        mb.add_header("Content-Disposition", "attachment", filename=filename)
-        msg.attach(mb)
+        #with open(filepath, "rb") as f:
+            #mb = MIMEApplication(f.read())
+        #mb.add_header("Content-Disposition", "attachment", filename=filename)
+        #msg.attach(mb)
+        if file_01 == True:
+            with open(filepath01, "rb") as f:
+                mb01 = MIMEApplication(f.read())
+            mb01.add_header("Content-Disposition", "attachment", filename=filename01)
+            msg.attach(mb01)
+        if file_02 == True:
+            with open(filepath02, "rb") as f:
+                mb02 = MIMEApplication(f.read())
+            mb02.add_header("Content-Disposition", "attachment", filename=filename02)
+            msg.attach(mb02)
+        if file_03 == True:
+            with open(filepath03, "rb") as f:
+                mb03 = MIMEApplication(f.read())
+            mb03.add_header("Content-Disposition", "attachment", filename=filename03)
+            msg.attach(mb03)
+        if file_04 == True:
+            with open(filepath04, "rb") as f:
+                mb04 = MIMEApplication(f.read())
+            mb04.add_header("Content-Disposition", "attachment", filename=filename04)
+            msg.attach(mb04)
+        if file_05 == True:
+            with open(filepath05, "rb") as f:
+                mb05 = MIMEApplication(f.read())
+            mb05.add_header("Content-Disposition", "attachment", filename=filename05)
+            msg.attach(mb05)
+        if file_06 == True:
+            with open(filepath06, "rb") as f:
+                mb06 = MIMEApplication(f.read())
+            mb06.add_header("Content-Disposition", "attachment", filename=filename06)
+            msg.attach(mb06)
+        if file_07 == True:
+            with open(filepath07, "rb") as f:
+                mb07 = MIMEApplication(f.read())
+            mb07.add_header("Content-Disposition", "attachment", filename=filename07)
+            msg.attach(mb07)
+        if file_08 == True:
+            with open(filepath08, "rb") as f:
+                mb08 = MIMEApplication(f.read())
+            mb08.add_header("Content-Disposition", "attachment", filename=filename08)
+            msg.attach(mb08)
+        if file_09 == True:
+            with open(filepath09, "rb") as f:
+                mb09 = MIMEApplication(f.read())
+            mb09.add_header("Content-Disposition", "attachment", filename=filename09)
+            msg.attach(mb09)
+        if file_10 == True:
+            with open(filepath10, "rb") as f:
+                mb10 = MIMEApplication(f.read())
+            mb10.add_header("Content-Disposition", "attachment", filename=filename10)
+            msg.attach(mb10)
+        if file_11 == True:
+            with open(filepath11, "rb") as f:
+                mb11 = MIMEApplication(f.read())
+            mb11.add_header("Content-Disposition", "attachment", filename=filename11)
+            msg.attach(mb11)
+        if file_12 == True:
+            with open(filepath12, "rb") as f:
+                mb12 = MIMEApplication(f.read())
+            mb12.add_header("Content-Disposition", "attachment", filename=filename12)
+            msg.attach(mb12)
 
         #メールを送付
         s = smtplib.SMTP(smtp_server, smtp_port)
@@ -54,3 +155,5 @@ def app():
         s.login(smtp_user, smtp_password)
         s.sendmail(from_address, to_address, msg.as_string())
         s.quit()
+
+        st.write("Transmission completed !")
